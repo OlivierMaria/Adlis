561b135bcbd6e0235203f0590f08c9832ca696e6
OlivierMaria <olivier-maria@hotmail.com>
2023-06-26 15:50:30 +0200

    ajustements

--- back/app/models/current.rb
+++
@@ -1,8 +0,0 @@
-class Current < ActiveSupport::CurrentAttributes
-  attribute :session, :user
-  attribute :user_agent, :ip_address
-
-  def session=(session)
-    super; self.user = session.user
-  end
-end
