d633c0920a8276b23f01e129d99f918801945d4c
Akvayli06 <benidirifaouzi@gmail.com>
2023-06-29 13:14:04 +0200

    styled profil page

--- front/src/components/Form.js
+++ front/src/components/Form.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import bookImage from "../assets/img/book-img.png";
 
 const Form = (props) => {
   const handleError = (errors) => {};
@@ -12,8 +13,8 @@ const Form = (props) => {
     button,
     usernamePlaceholder,
     emailPlaceholder,
-    paswwordPlaceholder,
     passwordConfirmationPlaceholder,
+    page,
   } = props;
 
   const {
@@ -53,89 +54,106 @@ const Form = (props) => {
   };
 
   return (
-    <div className="form-container">
-      <form
-        className="form"
-        onSubmit={handleSubmit(handleSubmitRequest, handleError)}
-      >
-        {username && (
-          <>
-            <label>{username}</label>
-            <input
-              type="text"
-              placeholder={usernamePlaceholder}
-              {...register("username", validOptions.username)}
-            />
-            <small className="text-danger">
-              {errors?.username && errors.username.message}
-            </small>
-          </>
-        )}
+    <div className="flex">
+      <div className="w-1/2 pr-8">
+        <img src={bookImage} alt="Book" className="h-full" />
+      </div>
+      <div className="w-1/2 pl-8">
+        <div className="form-container">
+          <form
+            className="form"
+            onSubmit={handleSubmit(handleSubmitRequest, handleError)}
+          >
+            {username && (
+              <>
+                <label>{username}</label>
+                <input
+                  type="text"
+                  placeholder={usernamePlaceholder}
+                  {...register("username", validOptions.username)}
+                />
+                <small className="text-danger">
+                  {errors?.username && errors.username.message}
+                </small>
+              </>
+            )}
 
-        {email && (
-          <>
-            <label>{email}</label>
-            <input
-              type="email"
-              placeholder={emailPlaceholder}
-              {...register("email", validOptions.email)}
-            />
-            <small className="text-danger">
-              {errors?.email && errors.email.message}
-            </small>
-          </>
-        )}
+            {email && (
+              <>
+                <label>{email}</label>
+                <input
+                  type="email"
+                  placeholder={emailPlaceholder}
+                  {...register("email", validOptions.email)}
+                />
+                <small className="text-danger">
+                  {errors?.email && errors.email.message}
+                </small>
+              </>
+            )}
 
-        {password && (
-          <>
-            <label>{password}</label>
-            <input
-              type="password"
-              placeholder={paswwordPlaceholder}
-              {...register("password", validOptions.password)}
-            />
-            <small className="text-danger">
-              {errors?.password && errors.password.message}
-            </small>
-          </>
-        )}
-        {passwordConfirmation && (
-          <>
-            <label>{passwordConfirmation}</label>
-            <input
-              type="password"
-              placeholder={passwordConfirmationPlaceholder}
-              {...register(
-                "password_confirmation",
-                validOptions.password_confirmation
-              )}
-            />
-            <small className="text-danger">
-              {errors?.password_confirmation &&
-                errors.password_confirmation.message}
-            </small>
-          </>
-        )}
-        {Conditions && (
-          <>
-            <label htmlFor="accept_conditions">{Conditions}</label>
-            <input
-              type="checkbox"
-              id="accept_conditions"
-              {...register("accept_conditions", validOptions.accept_conditions)}
-            />
+            {password && (
+              <>
+                <label>{password}</label>
+                <input
+                  type="password"
+                  // placeholder={passwordPlaceholder}
+                  {...register("password", validOptions.password)}
+                />
 
-            <small className="text-danger">
-              {errors?.accept_conditions && errors.accept_conditions.message}
-            </small>
-          </>
-        )}
-        {button && (
-          <>
-            <button>{button}</button>
-          </>
-        )}
-      </form>
+                {page === "signin" && errors.password && (
+                  <>
+                    {errors.password.type === "required" && (
+                      <small className="text-danger">mot de passe requis</small>
+                    )}
+                  </>
+                )}
+              </>
+            )}
+
+            {passwordConfirmation && (
+              <>
+                <label>{passwordConfirmation}</label>
+                <input
+                  type="password"
+                  placeholder={passwordConfirmationPlaceholder}
+                  {...register(
+                    "password_confirmation",
+                    validOptions.password_confirmation
+                  )}
+                />
+                <small className="text-danger">
+                  {errors?.password_confirmation &&
+                    errors.password_confirmation.message}
+                </small>
+              </>
+            )}
+            {Conditions && (
+              <>
+                <label htmlFor="accept_conditions">{Conditions}</label>
+                <input
+                  type="checkbox"
+                  id="accept_conditions"
+                  {...register(
+                    "accept_conditions",
+                    validOptions.accept_conditions
+                  )}
+                />
+
+                <small className="text-danger">
+                  {errors?.accept_conditions &&
+                    errors.accept_conditions.message}
+                </small>
+              </>
+            )}
+            {button && (
+              <>
+                <button>{button}</button>
+              </>
+            )}
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
